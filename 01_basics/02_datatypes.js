adf31cb8bdecd8a04cc7d770dc1e5f21021413fe
itsAlenGit <krishna43kashyap@gmail.com>
2025-03-01 13:15:11 +0000

    Data Types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,6 @@
+"use strict";// this mean use all the older code as newwe version
+
+//alert(3+3);//we are using node js not browser
+
+console.log(typeof undefined);// would ne undefined;
+console.log(typeof null);//object
