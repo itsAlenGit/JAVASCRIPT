c900cb7f90b7fa2db7f62236dfbef93531aae2f2
itsAlenGit <krishna43kashyap@gmail.com>
2025-03-01 12:34:17 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 123345;
+let accountEmail = "krishna43kashyap@gmail.com";//block scope specific {},value will be abvailable within the block
+var accoountPass = "1234";//anywhere we can change
+accountCitty = "Agra";//it is also allowed but not recommended
+let accouontAdd;//undefined
+
+console.table([accountId,accoountPass,accountEmail,accountCitty,accouontAdd]);
