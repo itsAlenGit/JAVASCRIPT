79144b1f9ec80b939018e2a93fc5cb2c653142fc
itsAlenGit <krishna43kashyap@gmail.com>
2025-03-01 14:10:02 +0000

    Conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,6 @@
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+
+console.log("1"+2+2); //jo pehle jis type ka ka usi type me sab convert hoga like "1" string h to sb stiong me convert hpoga
+console.log(1+2+"2");//isme pehna number num h to waki sb as number treat honge
